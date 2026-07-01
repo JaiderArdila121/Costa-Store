ff4fa131c45b7f67d03bb79017d3ee7adadf072b
jaider ardila <jaiderardila2514@gmail.com>
2026-06-30 20:59:40 -0500

    Optimizar imagenes de productos5

--- data/products.js
+++ data/products.js
@@ -39,6 +39,26 @@ const products = [
     
 
   },
+  {
+    id: 5,
+    name: "Combo Hidrolavadora + Aspiradora Portatil",
+    category: "Hogar",
+    price: "$100.000",
+    images: ["/productos/hidro/hidro1.png","/productos/hidro/hidro2.png","/productos/hidro/hidro3.png","/productos/hidro/hidro4.png"],
+    description:"pistola hidrolavadora, 2 baterías 48Vh Max, maletín de transporte, extensión de tubo, conexión de agua, manguera de 5 metros, filtro de agua, boquillas intercambiables de 0° y 40°, recipiente para jabón, adaptador de carga y manual de instrucciones y aspiradora portatil"
+    
+
+  },
+  {
+    id: 6,
+    name: "Lonchera Metálica Porta Calentadora Eléctrica De Comida",
+    category: "Hogar",
+    price: "$70.000",
+    images: ["/productos/lonche/lonchera1.png","/productos/lonche/lonchera2.png","/productos/lonche/lonchera3.png","/productos/lonche/lonchera4.png"],
+    description:"Electric Lunch Box, la práctica lonchera que ayudará a calentar tus alimentos y a qué permanezcan calientes y frescos por más tiempo.Ideal para llevar al trabajo, universidad o colegio.Con recipientes en aleación de aluminio el cual soporta altas temperaturas y hace que los alimentos se mantengan calientes por mucho más tiempo.Botón de encendido y apagado"
+    
+
+  },
 ];
 
 export default products;
